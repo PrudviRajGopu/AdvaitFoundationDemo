22eee93f1af3c07d9265df0f02dc780ce1202ce3
Prudvi Raj <prudvi.raj@virtualdentalcare.com>
2024-05-04 21:21:43 +0530

    Initial Commit

---
+++ AdvaitFoundationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvaitFoundationDemo
+//
+//  Created by Gopu on 04/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
